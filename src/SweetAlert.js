bf72af2d372e55c7ddca76a16bc417a328e269a1
Bill <baotuan0.0@gmail.com>
2018-10-31 23:32:23 +0700

    Change to Prop types

--- src/SweetAlert.js
+++ src/SweetAlert.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import swal from 'sweetalert';
 import pick from 'lodash.pick';
 import mousetrap from 'mousetrap';
